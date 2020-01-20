c29645444ec1c9d050d1d9f48f95db262d364a98
chrapekuba@gmail.com <chrapekuba@gmail.com>
2020-01-20 06:41:47 +0100

    Added public folder for source control

---
+++ public/component---src-pages-404-js-8442ef67d4deae9db29c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("Bl7J"),c=n("vrFN");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(c.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"Just a basic huddle fail"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-8442ef67d4deae9db29c.js.map
---
+++ public/component---src-pages-index-js-c41e5acfdf65ca4399b9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(e,t,n){"use strict";n.r(t);var l=n("q1tI"),a=n.n(l),o=(n("Wbzz"),n("Bl7J")),r=n("vrFN");t.default=function(){return a.a.createElement(o.a,null,a.a.createElement(r.a,{title:"Home"}),a.a.createElement("h1",null,"Hi people"),a.a.createElement("p",null,"Welcome to your new Gatsby site."),a.a.createElement("p",null,"Now go build something great."))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-c41e5acfdf65ca4399b9.js.map
